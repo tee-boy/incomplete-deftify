8b1bbd968ff8bbea0700c6078136531394d0d2b3
Tochukwu Emmanuel <125735714+tee-boy@users.noreply.github.com>
2023-08-05 07:46:18 -0700

    Add files via upload

---
+++ Deftify Project/index.js
@@ -0,0 +1,17 @@
+// function whosPaying(names) {
+    
+//     var numberOfPeople = names.length;
+//     var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+//     var randomPerson = names[randomPersonPosition];
+    
+//     return randomPerson + " is going to buy lunch today!";
+    
+// }
+// var name =["emmanuel", "joshua", "Mrs Vicky", "Divine","Johnpaul", "esther"];
+
+// function billsOn(name) {
+//     var nameOfPerson = name.length;
+//     var randomPersonName = Math.floor(Math.random() * nameOfPerson);
+//     var randomPerson = name[randomPersonName]
+//     alert(` ${randomPerson} is going to buy launch today`)
+// }
